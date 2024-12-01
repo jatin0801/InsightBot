2062ab83a38a6c20153336fd90ab98eabb429033
Jatin Chhabria <jatinchhabria00@gmail.com>
2024-11-30 19:07:22 -0500

    updated port for deployment

--- InsightBot_web/app.py
+++ InsightBot_web/app.py
@@ -90,4 +90,5 @@ def ask_question():
     return jsonify({"question": question, "answer": answer})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(port=port, debug=False)
